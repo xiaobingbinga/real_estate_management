c5d0ba04f099353119d3350eec500363164b4490
柳宇 <3400947454@qq.com>
2020-08-12 20:21:14 +0800

    文件上传和导包

--- house-api/src/test/java/com/xyetang9/house/houseapi/HouseApiApplicationTests.java
+++ house-api/src/test/java/com/xyetang9/house/houseapi/HouseApiApplicationTests.java
@@ -1,13 +1,18 @@
 package com.xyetang9.house.houseapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class HouseApiApplicationTests {
 
+    @Autowired
+    RedisTemplate redisTemplate;
     @Test
-    void contextLoads() {
+     public void addObject(){
+        redisTemplate.opsForZSet();
     }
 
 }
