b55000162f9243dac20b60901fff9272122ef36d
天冬 <z13455340468>
2020-08-12 20:43:04 +0800

    添加分包测试包

---
+++ demo/demo-zy/src/main/java/com/xuetang9/demozy/DemoZyApplication.java
@@ -0,0 +1,13 @@
+package com.xuetang9.demozy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoZyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoZyApplication.class, args);
+    }
+
+}
---
+++ demo/demo-zy/src/test/java/com/xuetang9/demozy/DemoZyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuetang9.demozy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoZyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
