d01f6e6b06c6d98b4041342097869778f06006b5
天冬 <z13455340468>
2020-08-13 15:56:57 +0800

    增加房产相关子项目

---
+++ house-houses/src/main/java/com/xuetang9/househouses/HouseHousesApplication.java
@@ -0,0 +1,13 @@
+package com.xuetang9.househouses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseHousesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseHousesApplication.class, args);
+    }
+
+}
---
+++ house-houses/src/test/java/com/xuetang9/househouses/HouseHousesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuetang9.househouses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseHousesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
