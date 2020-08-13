676fbb8118f74905df9314de33229cd8399d3f81
天冬 <z13455340468>
2020-08-13 16:13:26 +0800

    修改房产相关子项目

---
+++ house-houses/src/main/java/com/xuetang9/househouses/web/HouseController.java
@@ -0,0 +1,19 @@
+package com.xuetang9.househouses.web;
+
+
+import com.xyetang9.house.houseapi.web.PropertiesController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @ClassName: HouseController
+ * @Description: TODO
+ * @Author: 天冬
+ * @Date: 2020-08-13 16:04
+ * @Address: 成都市锦江区西部国际金融中心老九学堂
+ * @Version: v1.0
+ */
+public class HouseController {
+
+    @Autowired
+    PropertiesController propertiesController;
+}
