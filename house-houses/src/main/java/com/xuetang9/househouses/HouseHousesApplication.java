30909b4fd81062e23d386688d82b92011571a34e
天冬 <z13455340468>
2020-08-14 14:59:57 +0800

    完成高可用注册中心、负载均衡、熔断器、断路器

--- house-houses/src/main/java/com/xuetang9/househouses/HouseHousesApplication.java
+++ house-houses/src/main/java/com/xuetang9/househouses/HouseHousesApplication.java
@@ -3,15 +3,27 @@ package com.xuetang9.househouses;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableCaching
-@EnableEurekaClient
-@SpringBootApplication
+//@EnableEurekaClient
+//@EnableCircuitBreaker
+//@SpringBootApplication
+@SpringCloudApplication
 public class HouseHousesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HouseHousesApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
