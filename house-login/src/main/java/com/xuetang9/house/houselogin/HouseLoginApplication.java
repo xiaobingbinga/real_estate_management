cade09dfa2905ffaa0bfc7969eb62601a11726ea
VERLAAT <787521074@qq.com>
2020-08-16 19:10:32 +0800

    登录

--- house-login/src/main/java/com/xuetang9/house/houselogin/HouseLoginApplication.java
+++ house-login/src/main/java/com/xuetang9/house/houselogin/HouseLoginApplication.java
@@ -1,11 +1,13 @@
 package com.xuetang9.house.houselogin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan("com.xuetang9.house.mapper")
 public class HouseLoginApplication {
 
     public static void main(String[] args) {
---
+++ house-login/src/main/java/com/xuetang9/house/houselogin/web/UserController.java
@@ -0,0 +1,19 @@
+package com.xuetang9.house.houselogin.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName LoginController
+ * @Description: TODO
+ * @Author Mr_W
+ * @Date 2020/8/16 17:56
+ * @Version V1.0
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+
+}
