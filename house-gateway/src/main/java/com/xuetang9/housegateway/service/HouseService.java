708e20726a798b9eaf01742a44843f75e31fa212
天冬 <z13455340468>
2020-08-18 11:55:49 +0800

    完善网关相关代码

--- house-gateway/src/main/java/com/xuetang9/housegateway/service/HouseService.java
+++ house-gateway/src/main/java/com/xuetang9/housegateway/service/HouseService.java
@@ -1,9 +1,6 @@
 package com.xuetang9.housegateway.service;
 
-import com.xuetang9.house.vo.JsonResult;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 /**
  * @ClassName: HouseService
@@ -13,9 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @Address: 成都市锦江区西部国际金融中心老九学堂
  * @Version: v1.0
  */
-@FeignClient("HOUSE-HOUSES")
+//@FeignClient("HOUSE-HOUSES")
 public interface HouseService {
-
-    @GetMapping("/properties/properties-info/{id}")
-    JsonResult houseInfoByCondition(@PathVariable("id") Integer id);
+//
+//    @GetMapping("/properties/properties-info/{id}")
+//    JsonResult houseInfoByCondition(@PathVariable("id") Integer id);
 }
--- house-gateway/src/main/java/com/xuetang9/housegateway/web/HouseInfoApiController.java
+++ house-gateway/src/main/java/com/xuetang9/housegateway/web/HouseInfoApiController.java
@@ -1,13 +1,5 @@
 package com.xuetang9.housegateway.web;
 
-import com.xuetang9.house.vo.JsonResult;
-import com.xuetang9.housegateway.service.HouseService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @ClassName: HouseInfoApiController
@@ -17,22 +9,22 @@ import org.springframework.web.client.RestTemplate;
  * @Address: 成都市锦江区西部国际金融中心老九学堂
  * @Version: v1.0
  */
-@RestController
-@RequestMapping("/")
+//@RestController
+//@RequestMapping("/")
 public class HouseInfoApiController {
 
-    @Autowired
-    private HouseService houseService;
-
-//    private final String URL = "http://HOUSE-HOUSES/properties/";
-//
 //    @Autowired
-//    RestTemplate restTemplate;
-
-    @GetMapping("/properties-info/{id}")
-    public JsonResult houseInfoByCondition(@PathVariable("id") Integer id){
-//        return restTemplate.getForObject(URL + "properties-info?id={0}",JsonResult.class,id);
-        JsonResult jsonResult = houseService.houseInfoByCondition(id);
-        return jsonResult;
-    }
+//    private HouseService houseService;
+//
+////    private final String URL = "http://HOUSE-HOUSES/properties/";
+////
+////    @Autowired
+////    RestTemplate restTemplate;
+//
+//    @GetMapping("/properties-info/{id}")
+//    public JsonResult houseInfoByCondition(@PathVariable("id") Integer id){
+////        return restTemplate.getForObject(URL + "properties-info?id={0}",JsonResult.class,id);
+//        JsonResult jsonResult = houseService.houseInfoByCondition(id);
+//        return jsonResult;
+//    }
 }
